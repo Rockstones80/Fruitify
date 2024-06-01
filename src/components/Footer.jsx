077276f9fc73e2090a2e561366c761b8b8ea895b
uzor samuel <samueluzor80@gmail.com>
2024-06-01 18:39:10 +0100

    forth

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,47 @@
-// import { Link } from "react-router-dom";
-// import Icons from "../assets/Icons.svg";
+import { Link } from "react-router-dom";
+import Icons from "../assets/Icons.svg";
 
-// const Footer = () => {
-//   const Links = [
-//     { title: "Home", path: "/" },
-//     { title: "About", path: "/" },
-//     { title: "Shop", path: "/" },
-//   ];
+const Footer = () => {
+  const Links = [
+    { title: "Home", path: "/" },
+    { title: "About", path: "/" },
+    { title: "Shop", path: "/" },
+  ];
 
-//   return (
-//     <div className=" border-t-[1px] border-slate-300 w-full py-12 mt-12 flex px-20 justify-between">
-//       <div className="flex flex-col gap-32">
-//         <p className="font-bold">Fruitify</p>
-//         <img src={Icons} alt="" />
-//       </div>
-//       <div className=" pr-28 flex gap-44">
-//         <div>
-//           <p className="mb-5 font-medium">Topic</p>
-//           <div className="flex flex-col gap-8">
-//             {Links.map((link, index) => (
-//               <Link key={index}>{link.title}</Link>
-//             ))}
-//           </div>
-//         </div>
-//         <div>
-//           <p className=" mb-5 font-medium">Topic</p>
-//           <div className="flex flex-col gap-8">
-//             {Links.map((link, index) => (
-//               <Link key={index}>{link.title}</Link>
-//             ))}
-//           </div>
-//         </div>
-//         <div>
-//           <p className=" mb-5 font-medium">Topic</p>
-//           <div className="flex flex-col gap-8">
-//             {Links.map((link, index) => (
-//               <Link key={index}>{link.title}</Link>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className=" border-t-[1px] border-slate-300 w-full py-12 mt-12 flex px-20 justify-between">
+      <div className="flex flex-col gap-32">
+        <p className="font-bold">Fruitify</p>
+        <img src={Icons} alt="" />
+      </div>
+      <div className=" pr-28 flex gap-44">
+        <div>
+          <p className="mb-5 font-medium">Topic</p>
+          <div className="flex flex-col gap-8">
+            {Links.map((link, index) => (
+              <Link key={index}>{link.title}</Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <p className=" mb-5 font-medium">Topic</p>
+          <div className="flex flex-col gap-8">
+            {Links.map((link, index) => (
+              <Link key={index}>{link.title}</Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <p className=" mb-5 font-medium">Topic</p>
+          <div className="flex flex-col gap-8">
+            {Links.map((link, index) => (
+              <Link key={index}>{link.title}</Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Footer;
+export default Footer;
